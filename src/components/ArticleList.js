2b7005f31ac5609962860d8fae66226f3fd596d7
Mark Ghaida <markghaida@gmail.com>
2021-02-01 18:12:17 -0500

    done

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Article from "./Article";
+
+// console.log(Article)
+function ArticleList({posts}){
+
+    const articleList = posts.map((post) => (
+
+        <Article
+            key={post.id}
+            title={post.title}
+            date={post.date}
+            preview={post.preview}
+        />
+    ))
+
+    return<main>{articleList}</main>
+}
+
+export default ArticleList; 
